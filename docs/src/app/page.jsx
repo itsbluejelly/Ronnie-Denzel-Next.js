ad28591ea2dcc0439b9cdcb8b19c2891cbf5a3d9
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-07 22:44:27 +0300

    added routes and pages

--- docs/src/app/page.jsx
+++ docs/src/app/page.jsx
@@ -1,7 +1,5 @@
-export default function Home() {
+export default function HomePage() {
   return (
-    <p>
-      Hello World
-    </p>
+    <main>HomePage</main>
   )
 }
---
+++ docs/src/app/tickets/page.jsx
@@ -0,0 +1,3 @@
+export default function TicketsPage(){
+    return <main>Tickets Page</main>
+}
