3368eb17b843e2a8cb6cc6b1ce8eabf90ac712e1
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-07 23:03:38 +0300

    added navbar component

---
+++ docs/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+// IMPORTING NECESSARY FILES
+    // IMPORTING MODULES
+import { Link } from 'next/link'
+
+// A NAVBAR FUNCTION THAT GENERATES A NAVBAR ON DEFAULT EXPORT
+export default function Navbar(){
+    return <p>Hello World</p>
+}
