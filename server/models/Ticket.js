4f2114a2515dc63798229cafd45a69b53f729b1a
itsbluejelly <ronniedenzel0@gmail.com>
2023-10-10 06:51:17 +0300

    developed a ticket model

---
+++ server/models/Ticket.js
@@ -0,0 +1,30 @@
+// IMPORTING NECESSARY FILES
+    // IMPORT NECESSARY MODULES
+const mongoose = require('mongoose')
+
+// INSTANTIATE A SCHEMA FROM MONGOOSE
+const Schema = mongoose.Schema
+
+// DEFINING THE TICKET SCHEMA
+const TicketSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, "Your ticket must have a title"]
+    },
+
+    body: { type: String },
+    
+    priority: {
+        type: String,
+        required: [true, "Your ticket must have a level of priority"],
+        default: "low"
+    },
+
+    email: {
+        type: String,
+        required: [true, "Your ticket must be addressed to an email"]
+    },
+})
+
+// EXPORTING A TICKET MODEL FROM THE SCHEMA
+module.exports = mongoose.model("Ticket", TicketSchema)
